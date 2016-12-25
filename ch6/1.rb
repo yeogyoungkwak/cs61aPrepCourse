54e7d19cf371d48e2a926bd9ab784f673780af94
Your Name <yeogyoungkwak@gmail.com>
2016-12-24 18:33:22 -0800

    add ch 6 content

--- ch6/1.rb
+++ ch6/1.rb
@@ -1,1 +1,9 @@
-puts "this is a test"
+# Angry Boss #
+
+puts "Oh. It's you. What do want?"
+
+ans = gets.chomp
+
+str_ans = ans.to_s
+
+puts "WHAT DO YOU MEAN YOU WANT " + str_ans.upcase + "?! YOU'RE FIRED!!"
---
+++ ch6/2.rb
@@ -0,0 +1,9 @@
+# Table of Contents #
+
+line_width = 30
+
+puts('Table of Contents'.center(60))
+
+puts('Chapter 1: Getting Started'.ljust(line_width) + 'page 1'.rjust(line_width))
+puts('Chapter 2: Numbers'.ljust(line_width) + 'page 9'.rjust(line_width))
+puts('Chapter 3: Letters'.ljust(line_width) + 'page 13'.rjust(line_width))
